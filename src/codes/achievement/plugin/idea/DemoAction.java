05f12f7aadec03d5ab0ec7a66d627c7108aea2ff
Dozer <mail@dozer.cc>
2015-02-03 09:23:28 +0800

    add demo action

---
+++ src/codes/achievement/plugin/idea/DemoAction.java
@@ -0,0 +1,19 @@
+package codes.achievement.plugin.idea;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/**
+ * Dozer @ 2015-02
+ * mail@dozer.cc
+ * http://www.dozer.cc
+ */
+public class DemoAction extends AnAction {
+	public void actionPerformed(AnActionEvent e) {
+		// TODO: insert action logic here
+	}
+
+	@Override public void update(AnActionEvent e) {
+		super.update(e);
+	}
+}
