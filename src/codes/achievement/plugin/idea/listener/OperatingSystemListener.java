80dcfed0c4c9e132562b6b12648520b162b0b826
Dozer <mail@dozer.cc>
2015-02-10 21:57:40 +0800

    add version

--- src/codes/achievement/plugin/idea/listener/OperatingSystemListener.java
+++ src/codes/achievement/plugin/idea/listener/OperatingSystemListener.java
@@ -9,16 +9,18 @@ import codes.achievement.plugin.idea.storage.EventQueue;
  * http://www.dozer.cc
  */
 public class OperatingSystemListener implements Listener {
-    private static String OS = System.getProperty("os.name").toLowerCase();
+    private final static String OS = System.getProperty("os.name").toLowerCase();
+    private final static String VERSION = System.getProperty("os.version").toLowerCase();
+
 
     @Override
     public void register() {
         if (isWindows()) {
-            EventQueue.getCurrent().add(new EventDto("os-windows"));
+            EventQueue.getCurrent().add(new EventDto("os-windows-" + VERSION));
         } else if (isMac()) {
-            EventQueue.getCurrent().add(new EventDto("os-mac"));
+            EventQueue.getCurrent().add(new EventDto("os-mac-" + VERSION));
         } else if (isUnix()) {
-            EventQueue.getCurrent().add(new EventDto("os-unix"));
+            EventQueue.getCurrent().add(new EventDto("os-unix-" + VERSION));
         } else {
             EventQueue.getCurrent().add(new EventDto("os-other"));
         }
